33645fb6781221d9d1ef4ab8200ec80d5bd39110
savyez <visnuk252@gmail.com>
2026-04-14 16:15:12 +0530

    feat: Add transaction summary for current user

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import CategoryViewSet, TransactionViewSet
+from .views import (
+        CategoryViewSet, TotalExpenseViewSet, TotalIncomeViewSet, TransactionSummaryViewSet, 
+        TransactionTotalViewSet, TransactionViewSet, NetBalanceViewSet 
+        )
 
 urlpatterns = [
     path('categories/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'}), name='category-list'),
     path('categories/<uuid:pk>/', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='category-detail'),
     path('transactions/', TransactionViewSet.as_view({'get': 'list', 'post': 'create'}), name='transaction-list'),
     path('transactions/<uuid:pk>/', TransactionViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='transaction-detail'),
+    path('transactions/summary/', TransactionSummaryViewSet.as_view({'get': 'list'}), name='transaction-summary'),
+    path('transactions/summary/total/', TransactionTotalViewSet.as_view({'get': 'list'}), name='transaction-total'),
+    path('transactions/summary/total/income/', TotalIncomeViewSet.as_view({'get': 'list'}), name='total-income'),
+    path('transactions/summary/total/expense/', TotalExpenseViewSet.as_view({'get': 'list'}), name='total-expense'),
+    path('transactions/summary/net-balance/', NetBalanceViewSet.as_view({'get': 'list'}), name='net-balance'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
 from .serializers import CategorySerializer, TransactionSerializer
 from .models import Category, Transaction
+from django.db.models import F, Sum
+from rest_framework.response import Response
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -28,3 +29,91 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class TransactionSummaryViewSet(viewsets.ViewSet):
+    permission_class = [IsAuthenticated]
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(user=request.user)
+        summary = (
+            transactions
+            .values('category__name', transaction_type=F('type'))
+            .annotate(total=Sum('amount'))
+            .order_by('-total')
+        )
+        return Response({
+            'message': 'Successfully calculated summary',
+            'data': {
+                'user': request.user.username,
+                'summary': summary
+            }
+        })
+
+
+class TransactionTotalViewSet(viewsets.ViewSet):
+    permission_class = [IsAuthenticated]
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(user=request.user)
+        total = transactions.aggregate(total=Sum('amount'))['total'] or 0
+        return Response({
+            'message': 'Successfully calculated total income and expense',
+            'data':{
+                'user': request.user.username,
+                'transaction_count': transactions.count(),
+                'total': total
+                }})
+
+
+class TotalIncomeViewSet(viewsets.ViewSet):
+    permission_class = [IsAuthenticated]
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(user=request.user, type='income')
+        total = transactions.aggregate(total=Sum('amount'))['total'] or 0
+        return Response({
+            'message': 'Successfully calculated total income',
+            'data': {
+                'user': request.user.username,
+                'total_income': total
+            }
+        })
+
+
+class TotalExpenseViewSet(viewsets.ViewSet):
+    permission_class = [IsAuthenticated]
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(user=request.user, type='expense')
+        total = transactions.aggregate(total=Sum('amount'))['total'] or 0
+        return Response({
+            'message': 'Successfully calculated total expense',
+            'data': {
+                'user': request.user.username,
+                'total_expense': total
+            }
+        })
+
+
+class NetBalanceViewSet(viewsets.ViewSet):
+    permission_class = [IsAuthenticated]
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(user=request.user)
+        total_income = transactions.filter(type='income').aggregate(total=Sum('amount'))['total'] or 0
+        total_expense = transactions.filter(type='expense').aggregate(total=Sum('amount'))['total'] or 0
+        net_balance = total_income - total_expense
+        if net_balance >= 0:
+            balance_status = 'Cheers! You are in surplus.'
+        else:
+            balance_status = 'Oops! You are in debt.'
+
+        return Response({
+            'message': 'Successfully calculated net balance',
+            'data': {
+                'user': request.user.username,
+                'net_balance': net_balance,
+                'balance_status': balance_status
+            }
+        })
